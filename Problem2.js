6b3a4c7032bf996a6dc7dc4e2179e96932191ce7
leejr1983 <leejr1983@yahoo.com>
2019-01-16 11:32:04 -0600

    classwork

---
+++ Problem2.js
@@ -0,0 +1,38 @@
+// Problem 2:
+// //
+// // Put the start of your program in a main function.
+// // Ask the user to enter a 3 digit binary number.
+// // Create a switch case that prints 1 if you enter 1, 2 if you enter 10,
+// // 3 if you enter 11, 4 if you enter 100, and 5 if you enter 101.
+
+main()
+
+function main() {
+
+    var expr = prompt("Please enter 1, 10, 11, 100, or 101 for equivalent number.");
+
+    switch (expr) {
+        case "1":
+            console.log("1");
+            break;
+        case "10":
+            console.log("2");
+            break;
+        case "11":
+            console.log("3");
+            break;
+        case "100":
+            console.log("4")
+            break;
+        case "101":
+            console.log("5")
+    }
+
+
+
+
+
+
+
+
+}
